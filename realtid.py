5231a5dbea4f3405b2e4c52fee0e9311709b2508
Linus W <linus@mumindalen.net>
2015-04-16 11:19:45 +0000

    Include SJ, beginning to prepare for breaking out stuff into modules

--- realtid.py
+++ realtid.py
@@ -5,11 +5,22 @@ import urllib2
 import unicodedata
 import serial
 import ConfigParser
+import dateutil.parser
 import badge
 
 # Parse the config
 config = ConfigParser.ConfigParser()
 config.read('config')
+
+#########################
+
+# Setup, and open, the serial device for the display
+dev = config.get('DISPLAY','SerialPort')
+speed = config.get('DISPLAY','SerialSpeed')
+ser = serial.Serial(dev, speed)
+
+
+########################
 
 # Setup data source for SL (TBD: Extract into a module once we have multiple sources)
 a=config.get('SL', 'Url')
@@ -17,11 +28,6 @@ b=config.get('SL', 'Key')
 c=config.getint('SL', 'siteid')
 d=config.getint('SL', 'TimeWindow')
 url = "%s?key=%s&siteid=%s&TimeWindow=%s" % (a,b,c,d)
-
-# Setup, and open, the serial device for the display
-dev = config.get('DISPLAY','SerialPort')
-speed = config.get('DISPLAY','SerialSpeed')
-ser = serial.Serial(dev, speed)
 
 # Fetch data from SL
 data = json.load(urllib2.urlopen(url))
@@ -31,8 +37,45 @@ msg=""
 for train in data['ResponseData']['Trains']:
 #  for (k, v) in train.items():
 #    print k, v
-  print "%s, %s (%s)" % (train['Destination'], train['DisplayTime'], train['Deviations'])
-  msg+= "%s, %s (%s) " % (train['Destination'], train['DisplayTime'], train['Deviations'])
+#  print "====================="
+#  print "%s, %s (%s)" % (train['Destination'], train['DisplayTime'], train['Deviations'])
+  msg+= "%s (SL) " % (train['DisplayTime'])
+
+
+###################################
+
+
+
+url=config.get('SJ', 'Url')
+key=config.get('SJ', 'Key')
+loc=config.get('SJ', 'Loc')
+dst=config.get('SJ', 'Dst')
+
+xml='<REQUEST> <LOGIN authenticationkey="%s" /> <QUERY objecttype="TrainAnnouncement" orderby="AdvertisedTimeAtLocation"> <FILTER> <AND> <EQ name="ActivityType" value="Avgang" /> <EQ name="LocationSignature" value="%s" /> <EQ name="InformationOwner" value="SJ" /> <LIKE name="ToLocation" value="/%s/" /> <OR> <AND> <GT name="AdvertisedTimeAtLocation" value="$dateadd(-00:15:00)" /> <LT name="AdvertisedTimeAtLocation" value="$dateadd(14:00:00)" /> </AND> <AND> <LT name="AdvertisedTimeAtLocation" value="$dateadd(00:30:00)" /> <GT name="EstimatedTimeAtLocation" value="$dateadd(-00:15:00)" /> </AND> </OR> </AND> </FILTER> <INCLUDE>AdvertisedTrainIdent</INCLUDE> <INCLUDE>AdvertisedTimeAtLocation</INCLUDE> <INCLUDE>TrackAtLocation</INCLUDE> <INCLUDE>ToLocation</INCLUDE> </QUERY> </REQUEST>' % (key, loc, dst)
+
+data = json.load(urllib2.urlopen(urllib2.Request(url=url, data=xml, headers={'Content-Type': 'text/xml'})))
+for d in data['RESPONSE']['RESULT'][0]['TrainAnnouncement']:
+  t=dateutil.parser.parse(d['AdvertisedTimeAtLocation'])
+  msg += "%s (SJ) " % (t.strftime("%H:%M"))
+
+#############################
+
+a=config.get('SL2', 'Url')
+b=config.get('SL2', 'Key')
+c=config.getint('SL2', 'siteid')
+d=config.get('SL2', 'TransportMode')
+e=config.getint('SL2', 'LineNumber')
+url = "%s?key=%s&siteid=%s&LineNumber=%s&TransportMode=%s" % (a,b,c,e,d)
+
+# Fetch data from SL
+data = json.load(urllib2.urlopen(url))
+
+# Concat the scrolling message
+for d in data['ResponseData']:
+  print d['Header']
+  msg+= d['Header']
+
+#############################
 
 # Normalize the unicode into plain ASCII (the display /can/ handle Unicode, TBD...)
 msg=unicodedata.normalize('NFKD', msg).encode('ascii','ignore')
